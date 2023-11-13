cb4165c3bb68e6b927290631a2e4370e146811dd
ek05ywof <gitos.rrze.fau.de:mubashir.hassan@fau.de>
2023-11-13 23:16:39 +0100

    Add new changes

---
+++ math_quiz_package/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-quiz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz.quiz:main',
+        ],
+    },
+)
